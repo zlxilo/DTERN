926811e065da0443417d0b597aad5dca9b4e2553
zlxilo <18784043006@163.com>
2025-03-10 19:34:55 +0800

    Update num_parameters.py

--- mmseg/models/decode_heads/utils/num_parameters.py
+++ mmseg/models/decode_heads/utils/num_parameters.py
@@ -5,8 +5,5 @@
 # LICENSE file in the root directory of this source tree.
 #
 
-## copy from: https://github.com/facebookresearch/astmt/blob/4d5d9bf96b94040ce5be4b12fdccd871e58b1cd3/fblib/util/model_resources/num_parameters.py#L9
-
-
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
