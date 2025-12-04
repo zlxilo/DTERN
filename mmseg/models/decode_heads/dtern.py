0222ac20c26cc4752cf68dc5b07f577fded714e6
zlxilo <18784043006@163.com>
2025-12-04 17:24:39 +0800

    Update dtern.py

--- mmseg/models/decode_heads/dtern.py
+++ mmseg/models/decode_heads/dtern.py
@@ -178,7 +178,193 @@ class Cluster_Block(nn.Module):
         out = self.cam(x)
         return self.Clustering(out)
 
+class Cluster_layer(nn.Module):
+    def __init__(self, dim, num_heads,  num_clusters,mlp_ratio=4., qkv_bias=True, 
+                 qk_scale=None, drop=0., attn_drop=0., drop_path=0.,
+                 act_layer=nn.GELU, norm_layer=nn.LayerNorm,t=3,inner_center = True,backbone='b1',cityscape=False):
+        super().__init__()
+        self.dim = dim
+        self.num_heads = num_heads
+        self.num_clusters = num_clusters
+        self.norm1 = norm_layer(dim)
+        self.inner_center = inner_center
 
+        self.sim_alpha = nn.Parameter(torch.ones(1))
+        self.sim_beta = nn.Parameter(torch.zeros(1))
+        self.clustering = Cluster_Block(dim,num_clusters,mlp_ratio=mlp_ratio,backbone=backbone,cityscape=cityscape) # 
+
+        self.q = nn.Linear(dim, dim)
+        self.k = nn.Linear(dim, dim)
+        self.v = nn.Linear(dim, dim)
+        self.attn_drop = attn_drop
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(0.)
+        self.mlp = Mlp(in_features=dim, hidden_features=dim*4, act_layer=act_layer, drop=drop)
+        self.prompt1 = torch.nn.parameter.Parameter(torch.randn(num_clusters, requires_grad=True)) 
+        self.top_down_transform1 = torch.nn.parameter.Parameter(torch.eye(num_clusters), requires_grad=True)
+        self.prompt2 = torch.nn.parameter.Parameter(torch.randn(num_clusters, requires_grad=True)) 
+        self.top_down_transform2 = torch.nn.parameter.Parameter(torch.eye(num_clusters), requires_grad=True)
+
+    def forward(self,x,H,W,z=None,mem=None,t=3):
+        '''
+            x:[b,n,c]
+            mem:[bt,n,c]
+        '''
+        res = x
+        assigned_results = None
+        if mem is not None:
+            x = torch.cat([mem.view(-1,t,mem.shape[-2],mem.shape[-1]),x.unsqueeze(1)],dim=1) #[b,t,n,c]
+            x = x.flatten(0,1) #[bt,n,c]
+            t = t+1
+        else:
+            t = 1
+    
+        # CAM
+        cluster_x_z = self.clustering(x,H,W) # [bt,num_clusters,n]
+        cluster_x_z = rearrange(cluster_x_z,"(b t) c n -> b t c n",b=res.shape[0])
+
+        cluster_x_z = cluster_x_z.permute(0,2,1,3).contiguous().flatten(2) # [b,num_clusters,tn]
+        
+        #SAM
+        if z is not None:
+            z = rearrange(z,'b c n -> b n c') #(b,tn,num_clusters)
+            cluster_x_z = rearrange(cluster_x_z,'b c n -> b n c') #(b,tn,num_clusters)
+            # select:
+            cos_sim = F.normalize(z, dim=-1) @ F.normalize(self.prompt1[None, ..., None], dim=1)  # B, N, 1 
+            mask = cos_sim.clamp(0, 1)  
+            z = z * mask    
+            z = z @ self.top_down_transform1 
+            cos_sim = F.normalize(cluster_x_z, dim=-1) @ F.normalize(self.prompt2[None, ..., None], dim=1)  # B, N, 1 
+            mask = cos_sim.clamp(0, 1)
+            cluster_x_z = cluster_x_z * mask
+
+            cluster_x_z = cluster_x_z @ self.top_down_transform2
+
+            cluster_x_z = (cluster_x_z + z)/2.
+            cluster_x_z = rearrange(cluster_x_z,"b n c -> b c n")
+
+        if self.inner_center:
+            center = rearrange(cluster_x_z,"b c (t n) -> b t c n",t = t)
+            assigned_results = center.clone()
+            center = center.softmax(dim=-1) 
+            center_x = rearrange(x,"(b t) n c -> b t n c",t=t)
+            center = center @ center_x 
+            cos_sim = torch.sigmoid( 
+                (self.sim_beta + self.sim_alpha * F.cosine_similarity(center[:,-1].unsqueeze(1),center[:,:-1],dim=-1))
+            ) #[b,t,num_cluster]
+            C_in = center[:,-1] + (cos_sim.unsqueeze(-1) * center[:,:-1]).sum(dim=1) 
+        
+        # FEM
+        C_in = self.norm1(C_in)
+        src = rearrange(res,"b n c -> n b c")
+        mem = rearrange(C_in, "b n c -> n b c")
+        out,_ = F.multi_head_attention_forward(
+            query=src,
+            key=mem,
+            value=mem,
+            embed_dim_to_check=x.shape[-1],
+            num_heads=self.num_heads,
+            q_proj_weight=self.q.weight,
+            k_proj_weight=self.k.weight,
+            v_proj_weight=self.v.weight,
+            in_proj_weight=None,
+            in_proj_bias=torch.cat([self.q.bias, self.k.bias, self.v.bias]),
+            bias_k=None,
+            bias_v=None,
+            add_zero_attn=False,
+            dropout_p=self.attn_drop,
+            out_proj_weight=self.proj.weight,
+            out_proj_bias=self.proj.bias,
+            use_separate_proj_weight=True,
+            training=self.training,
+            need_weights=not self.training
+        )
+        out = rearrange(out,"n b c -> b n c")
+        out = self.proj_drop(out)
+        
+        out = res + self.norm1(out)
+        out = out + self.norm1(self.mlp(out,H,W))
+        return out,cluster_x_z,assigned_results
+    
+
+class GTEM(nn.Module):
+    def __init__(self, dim, num_heads=8, num_classes=150, qkv_bias=True, qk_scale=None, T=3):
+        super().__init__()
+        self.num_heads = num_heads
+        self.num_classes = num_classes
+        head_dim = dim // num_heads
+        self.scale = qk_scale or head_dim ** -0.5
+
+        self.q = nn.Linear(dim, dim, bias=qkv_bias)
+        self.k = nn.Linear(dim, dim, bias=qkv_bias)
+
+        self.FCC = nn.Parameter(torch.zeros(1, T, num_classes, dim)) 
+        
+        self.VCC = nn.Parameter(torch.zeros(1, num_classes, dim)) 
+        
+        self.cross_attn = SelfAttentionBlockWithTime(
+            key_in_channels=dim,
+            query_in_channels=dim,
+            channels=dim,
+            out_channels=dim,
+            share_key_query=False,
+            query_downsample=None,
+            key_downsample=None,
+            key_query_num_convs=1,
+            value_out_num_convs=1,
+            key_query_norm=True,
+            value_out_norm=True,
+            matmul_norm=True,
+            with_out=True,
+            conv_cfg=None,
+            norm_cfg=dict(type='SyncBN', requires_grad=True),
+            act_cfg=dict(type='ReLU'))
+        
+        self.T = T
+        trunc_normal_(self.FCC, std=.02)
+        trunc_normal_(self.VCC, std=.02)
+        self.apply(self._init_weights) 
+        
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
+             
+    def forward(self, x):
+        b, t, c, h, w = x.size()
+        res = x
+        assert t == self.T, "Input tensor has wrong time"
+        x = x.flatten(3).transpose(-1, -2) #[b,t,n,c]
+        B, T, N, C = x.shape
+        FCC = self.FCC.expand(B, -1, -1, -1) # [B, T, num_classes, dim]
+        VCC = self.VCC.unsqueeze(1).expand(B, T, -1, -1) 
+        
+        # B, T, N, C = x.shape
+        q = self.q(x).reshape(B, T, N, self.num_heads, C // self.num_heads).permute(0, 1, 3, 2, 4)
+        k = self.k(FCC).unsqueeze(1).reshape(B, T, self.num_classes, self.num_heads, C // self.num_heads).permute(0, 1, 3, 2, 4) #[B, T, num_heads, num_classes, dim]
+        
+        k = k * self.scale
+        attn = (k @ q.transpose(-2, -1)).squeeze(2).transpose(-2, -1) 
+        
+        out_cls_mid = attn.permute(0, 1, 3, 2).reshape(b, t, -1, h, w)
+        x_cls = out_cls_mid.flatten(3).transpose(-1, -2)   # [b,t,n,c]
+        x_cls = x_cls.softmax(dim=-1)
+        cls = x_cls @ VCC
+        cls = cls.permute(0, 1, 3, 2).reshape(b, t, c, h, w) 
+        x2 = self.cross_attn(res, cls)
+        out = res + x2 
+
+        return out_cls_mid,out
 
 class DTERN(nn.Module):
     def __init__(self,dim=[64, 128, 320, 512], num_layers=1, t=3, time_decoder_layer=3,embedding_dim=256,num_classes = 124,
